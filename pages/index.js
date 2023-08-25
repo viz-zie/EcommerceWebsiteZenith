34b29839151c44cda1e165b4962e11704c3d04d9
vizzie <136495477+viz-zie@users.noreply.github.com>
2023-08-25 13:03:00 +0530

    webdev project complete

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+$(".dropdown-toggle").click(function()
+ {
+  $(".dropdown-menu").slideDown(500);    
+ });
+
+ 
+
+ $(".usn1").click(function()
+ {
+    $(".usn1").css("color","orange");
+ });
+
+ $(".usn2").click(function()
+ {
+    $(".usn2").css("color","orange");
+ });
+
+
+ $(".dropdown-toggle").click(function()
+ {
+  $(".dropdown-menu").slideUp(5500);    
+ });
+
+
+ $(".carousel-control-next").click( function(){
+
+    $(".carousel-item active img1").html("<img src='../pictures/v2.webp' class='d-block w-100 img2' alt=...>");
+
+
+ });
+
+
+ 
+
+ /*
+ $(".logout").click(function()
+ {
+    $(".logout").text("Good Bye !");
+ });
+ */
+
+
